627e7e2580d55f9167e5e678c76f444ef84dc93a
mateusfg7 <mateusfelipefg77@gmail.com>
2020-12-31 08:16:55 -0300

    server: use routes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,11 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
+import router from "./routes.ts";
+
 const PORT = config().PORT ?? 8000;
 
 const app = new Application();
-const router = new Router();
-
-router.get("/", (context) => {
-  context.response.body = "Hello Word!";
-});
-
-router.get("/test", (context) => {
-  context.response.body = "Test!";
-});
 
 app.use(router.routes());
 
