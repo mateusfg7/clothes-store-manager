95cbe20eea55059e3173dfad4d249fb6269aa7ec
mateusfg7 <mateusfelipefg77@gmail.com>
2020-12-31 07:47:44 -0300

    server: use .env configs

--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,7 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+const PORT = config().PORT ?? 8000;
 
 const app = new Application();
 
@@ -6,4 +9,4 @@ app.use((context) => {
   context.response.body = "Hello Word!";
 });
 
-app.listen({ port: 8000 });
+await app.listen({ port: Number(PORT) });
