6fb777823d07f11213e51b4a7c0410cdbd724a76
mateusfg7 <mateusfelipefg77@gmail.com>
2020-12-31 13:34:13 -0300

    server: update '/create' route
    Get the request body as json

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,11 +3,20 @@ import { Router } from "https://deno.land/x/oak/mod.ts";
 const router = new Router();
 
 router.get("/", (context) => {
-  context.response.body = "Hello Word!";
+  context.response.body = { message: "Hello word!" };
 });
 
-router.get("/test", (context) => {
-  context.response.body = "Test!";
+router.get("/create", async (context) => {
+  const request = await context.request.body({ type: "json" }).value;
+
+  if (typeof request != "object") {
+    console.log("Is an object");
+    context.response.status = 400;
+    return;
+  }
+
+  context.response.status = 200;
+  context.response.body = request;
 });
 
 export default router;
