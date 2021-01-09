97b2c4aeba0519661cf275ddf9f593e35c28964d
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-09 09:28:09 -0300

    web: create styles to index page

---
+++ web/src/styles/pages/index.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Dashboard = styled.main`
+  display: flex;
+  height: 100vh;
+  border: 1px solid red;
+`
+
+export const NavSection = styled.section`
+  border: 5px solid blue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  min-width: 14rem;
+
+  text-align: center;
+
+  img {
+    width: 6rem;
+    border-radius: 100%;
+  }
+
+  h1 {
+    margin-top: 1rem;
+    font-size: 1.2rem;
+  }
+
+  nav {
+    margin-top: 2rem;
+  }
+`
+
+export const MainSection = styled.section`
+  border: 5px solid green;
+  width: 100%;
+`
