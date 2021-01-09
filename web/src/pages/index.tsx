b0f7359dc603f6c34849d15857121d7a64c3a13b
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-09 09:28:32 -0300

    web: create initial layout on index page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { Dashboard, NavSection, MainSection } from '../styles/pages/index'
 
 const Home: React.FC = () => {
   return (
@@ -8,9 +11,26 @@ const Home: React.FC = () => {
         <title>Clothes Store Managers</title>
       </Head>
 
-      <main>
-        <h1>Clothes Store Manager</h1>
-      </main>
+      <Dashboard>
+        <NavSection>
+          <Image
+            src="/img/logo.jpg"
+            layout="intrinsic"
+            width={500}
+            height={500}
+          />
+          <h1>Brito Clothes</h1>
+          <p>Clothes manager dashboard</p>
+          <nav>
+            <ul>
+              <li>option 1</li>
+              <li>option 2</li>
+              <li>option 3</li>
+            </ul>
+          </nav>
+        </NavSection>
+        <MainSection></MainSection>
+      </Dashboard>
     </div>
   )
 }
