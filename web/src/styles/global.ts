ee7f3876b79a87d026149225a359b5ebee9ef9a1
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-09 09:27:20 -0300

    web: update global style
    Add root font-size

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -1,17 +1,16 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  :root {
+    font-size: 90%;
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
   body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    height: 100vh;
     background: ${props => props.theme.colors.background};
     font-family: 'Inter', sans-serif;
     color: ${props => props.theme.colors.text};
