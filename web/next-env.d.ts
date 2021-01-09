7ef6da4bfb4b363d159b4747adfe463514cb9b9b
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-09 09:26:18 -0300

    web: create clothes interface

--- web/next-env.d.ts
+++ web/next-env.d.ts
@@ -1,2 +1,18 @@
+/* eslint-disable camelcase */
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+interface Clothes {
+  id: number
+  product: string
+  brand: string
+  quantity: number
+  provider: string
+  price: number
+  current_inventory: number
+  size: string
+  input_values: number
+  output_values: number
+  createdAt: string
+  updatedAt: string
+}
