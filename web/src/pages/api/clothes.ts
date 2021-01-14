60ca81ae38dfaace556aa0e6ecf2d10d01666e28
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-14 07:40:56 -0300

    web: create serveless function to handle form submit

---
+++ web/src/pages/api/clothes.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import api from '@services/api'
+
+export default async (req: NextApiRequest, res: NextApiResponse): void => {
+  console.log(JSON.parse(req.body))
+
+  const response = await api.post('/clothes', JSON.parse(req.body))
+
+  res.status(response.status).send('')
+}
