2fc4b0b7c14f25c28d9c7d04eac1d0297e29fdd1
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-14 15:02:28 -0300

    web: remove global transition

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -10,7 +10,6 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     outline: none;
-    transition: 0.2s;
   }
   body {
     background: ${props => props.theme.colors.background};
