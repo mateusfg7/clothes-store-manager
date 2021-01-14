570938068ab79633a964fdac2f6469618a62b37a
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-14 14:39:20 -0300

    web: finish UX/UI of the page 'new'

--- web/src/pages/clothes/new.tsx
+++ web/src/pages/clothes/new.tsx
@@ -92,148 +92,254 @@ const Clothes: React.FC = () => {
           {/* <div className="shortcuts"></div> */}
           <div className="dashboard">
             <h1>Cadastrar nova roupa</h1>
-            <form onSubmit={handleSubmit}>
-              <div className="input-block">
-                <label htmlFor="product">Descrição do produto</label>
-                <input
-                  required
-                  id="product"
-                  value={product}
-                  onChange={event => setProduct(event.target.value)}
-                />
-              </div>
 
-              <div className="input-block">
-                <label htmlFor="brand">Marca</label>
-                <input
-                  required
-                  id="brand"
-                  value={brand}
-                  onChange={event => setBrand(event.target.value)}
-                />
-              </div>
+            <div className="sections">
+              <section className="content">
+                <form onSubmit={handleSubmit}>
+                  <div className="input-section">
+                    <div className="input-block">
+                      <label htmlFor="product">Descrição do produto</label>
+                      <input
+                        required
+                        id="product"
+                        value={product}
+                        onChange={event => setProduct(event.target.value)}
+                      />
+                    </div>
 
-              <div className="input-block">
-                <label htmlFor="quantity">Quantidade</label>
-                <input
-                  required
-                  type="number"
-                  id="quantity"
-                  value={quantity}
-                  onChange={event => setQuantity(Number(event.target.value))}
-                />
-              </div>
+                    <div className="input-block">
+                      <label htmlFor="brand">Marca</label>
+                      <input
+                        required
+                        id="brand"
+                        value={brand}
+                        onChange={event => setBrand(event.target.value)}
+                      />
+                    </div>
 
-              <div className="input-block">
-                <label htmlFor="provider">Fornecedor</label>
-                <input
-                  required
-                  id="provider"
-                  value={provider}
-                  onChange={event => setProvider(event.target.value)}
-                />
-              </div>
+                    <div className="input-block">
+                      <label htmlFor="quantity">Quantidade</label>
+                      <input
+                        required
+                        type="number"
+                        id="quantity"
+                        value={quantity}
+                        onChange={event =>
+                          setQuantity(Number(event.target.value))
+                        }
+                      />
+                    </div>
 
-              <div className="input-block">
-                <label htmlFor="price">Preço</label>
-                <input
-                  required
-                  type="number"
-                  id="price"
-                  value={price}
-                  onChange={event => setPrice(Number(event.target.value))}
-                />
-              </div>
+                    <div className="input-block">
+                      <label htmlFor="provider">Fornecedor</label>
+                      <input
+                        required
+                        id="provider"
+                        value={provider}
+                        onChange={event => setProvider(event.target.value)}
+                      />
+                    </div>
 
-              <div className="input-block">
-                <label>Tamanhos disponíveis</label>
-                <button
-                  type="button"
-                  className={`size-button ${sizeState.PP ? 'active' : ''}`}
-                  onClick={() => {
-                    handleSizeState('PP')
-                  }}
-                >
-                  PP
-                </button>
-                <button
-                  type="button"
-                  className={`size-button ${sizeState.P ? 'active' : ''}`}
-                  onClick={() => {
-                    handleSizeState('P')
-                  }}
-                >
-                  P
-                </button>
-                <button
-                  type="button"
-                  className={`size-button ${sizeState.M ? 'active' : ''}`}
-                  onClick={() => {
-                    handleSizeState('M')
-                  }}
-                >
-                  M
-                </button>
-                <button
-                  type="button"
-                  className={`size-button ${sizeState.G ? 'active' : ''}`}
-                  onClick={() => {
-                    handleSizeState('G')
-                  }}
-                >
-                  G
-                </button>
-                <button
-                  type="button"
-                  className={`size-button ${sizeState.GG ? 'active' : ''}`}
-                  onClick={() => {
-                    handleSizeState('GG')
-                  }}
-                >
-                  GG
-                </button>
-                <button
-                  type="button"
-                  className={`size-button ${sizeState.GGG ? 'active' : ''}`}
-                  onClick={() => {
-                    handleSizeState('GGG')
-                  }}
-                >
-                  GGG
-                </button>
-                <button
-                  type="button"
-                  className="reset-sizes"
-                  onClick={resetSizes}
-                >
-                  resetar
-                </button>
-              </div>
+                    <div className="input-block">
+                      <label>Tamanhos disponíveis</label>
+                      <div className="all-buttons">
+                        <div className="size-buttons">
+                          <button
+                            type="button"
+                            className={`size-button ${
+                              sizeState.PP ? 'active' : ''
+                            }`}
+                            onClick={() => {
+                              handleSizeState('PP')
+                            }}
+                          >
+                            PP
+                          </button>
+                          <button
+                            type="button"
+                            className={`size-button ${
+                              sizeState.P ? 'active' : ''
+                            }`}
+                            onClick={() => {
+                              handleSizeState('P')
+                            }}
+                          >
+                            P
+                          </button>
+                          <button
+                            type="button"
+                            className={`size-button ${
+                              sizeState.M ? 'active' : ''
+                            }`}
+                            onClick={() => {
+                              handleSizeState('M')
+                            }}
+                          >
+                            M
+                          </button>
+                          <button
+                            type="button"
+                            className={`size-button ${
+                              sizeState.G ? 'active' : ''
+                            }`}
+                            onClick={() => {
+                              handleSizeState('G')
+                            }}
+                          >
+                            G
+                          </button>
+                          <button
+                            type="button"
+                            className={`size-button ${
+                              sizeState.GG ? 'active' : ''
+                            }`}
+                            onClick={() => {
+                              handleSizeState('GG')
+                            }}
+                          >
+                            GG
+                          </button>
+                          <button
+                            type="button"
+                            className={`size-button ${
+                              sizeState.GGG ? 'active' : ''
+                            }`}
+                            onClick={() => {
+                              handleSizeState('GGG')
+                            }}
+                          >
+                            GGG
+                          </button>
+                        </div>
+                        <button
+                          type="button"
+                          className="reset-sizes"
+                          onClick={resetSizes}
+                        >
+                          resetar
+                        </button>
+                      </div>
+                    </div>
 
-              <div className="input-block">
-                <label htmlFor="inputValues">Valores de entrada</label>
-                <input
-                  required
-                  type="number"
-                  id="inputValues"
-                  value={inputValues}
-                  onChange={event => {
-                    setInputValues(Number(event.target.value))
-                  }}
-                />
-                <label htmlFor="outputValues">Valores de saida</label>
-                <input
-                  type="number"
-                  id="outputValues"
-                  value={outputValues}
-                  onChange={event => {
-                    setOutputValues(Number(event.target.value))
-                  }}
-                />
-              </div>
+                    <div className="input-block">
+                      <label htmlFor="inputValues">Valores de entrada</label>
+                      <input
+                        required
+                        type="number"
+                        id="inputValues"
+                        value={inputValues}
+                        onChange={event => {
+                          setInputValues(Number(event.target.value))
+                        }}
+                      />
+                      <label htmlFor="outputValues">Valores de saida</label>
+                      <input
+                        type="number"
+                        id="outputValues"
+                        value={outputValues}
+                        onChange={event => {
+                          setOutputValues(Number(event.target.value))
+                        }}
+                      />
+                      <label htmlFor="price">Preço</label>
+                      <input
+                        required
+                        type="number"
+                        id="price"
+                        value={price}
+                        onChange={event => setPrice(Number(event.target.value))}
+                      />
+                    </div>
+                  </div>
 
-              <button type="submit">Cadastrar</button>
-            </form>
+                  <button type="submit" className="submit">
+                    Cadastrar
+                  </button>
+                </form>
+              </section>
+
+              <section className="description">
+                <div className="description-texts">
+                  <div className="description-fieldset">
+                    <h4>Descrição do produto</h4>
+                    <p>
+                      faça uma descrição característica do produto, como o nome,
+                      material e cor.
+                    </p>
+                  </div>
+                  <div className="description-fieldset">
+                    <h4>Marca</h4>
+                    <p>Nome da marca do produto</p>
+                  </div>
+                  <div className="description-fieldset">
+                    <h4>Quantidade</h4>
+                    <p>Quantidade de peças adquiridas</p>
+                  </div>
+                  <div className="description-fieldset">
+                    <h4>Fornecedor</h4>
+                    <p>Pessoa/empresa fornecedora da peça</p>
+                  </div>
+                  <div className="description-fieldset">
+                    <h4>Valores de entrada</h4>
+                    <p>Lucro obtido</p>
+                  </div>
+                  <div className="description-fieldset">
+                    <h4>Valores de saida</h4>
+                    <p>Gastos de compra e manutenção</p>
+                  </div>
+                  <div className="description-fieldset">
+                    <h4>Preço</h4>
+                    <p>Valor a ser revendido</p>
+                  </div>
+                  <div className="description-fieldset">
+                    <h4>Tamanhos disponíveis</h4>
+                    <p>Tamanhos da peça comprada</p>
+                  </div>
+                </div>
+                <div className="table">
+                  <div className="column">
+                    <div className="title">PP</div>
+                    <div className="content">
+                      <div className="number">34</div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="title">P</div>
+                    <div className="content">
+                      <div className="number">36</div>
+                      <div className="number">38</div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="title">M</div>
+                    <div className="content">
+                      <div className="number">40</div>
+                      <div className="number">42</div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="title">G</div>
+                    <div className="content">
+                      <div className="number">44</div>
+                      <div className="number">46</div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="title">GG</div>
+                    <div className="content">
+                      <div className="number">48</div>
+                      <div className="number">50</div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="title">GGG</div>
+                    <div className="content">
+                      <div className="number">+</div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
           </div>
         </ExtendedMain>
       </Display>
--- web/src/styles/pages/new.ts
+++ web/src/styles/pages/new.ts
@@ -1,13 +1,176 @@
 import styled from 'styled-components'
+import { transparentize, mix } from 'polished'
 
 import { Main } from '@styles/global-components'
 
 export const ExtendedMain = styled(Main)`
-  form fieldset button.size-button {
-    opacity: 0.5;
+  div.dashboard {
+    div.sections {
+      display: flex;
+      justify-content: center;
 
-    &.active {
-      opacity: 1;
+      section {
+        width: 100%;
+        padding: 0.5rem;
+      }
+
+      section.content {
+      }
+
+      section.description {
+        opacity: 0.8;
+
+        width: 85%;
+
+        div.description-texts {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          gap: 0.9rem;
+
+          div.description-fieldset {
+            margin-bottom: 1rem;
+
+            p {
+              margin-top: 0.7rem;
+              font-size: 0.9rem;
+            }
+          }
+        }
+
+        div.table {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          border: 1px solid ${props => props.theme.colors.text};
+          border-radius: 0.5rem;
+
+          div.column {
+            flex: 1;
+            text-align: center;
+
+            & + div.column {
+              border-left: 1px solid ${props => props.theme.colors.text};
+            }
+
+            div.title {
+              padding: 0.5rem;
+            }
+
+            div.content {
+              display: flex;
+              justify-content: space-evenly;
+              align-items: center;
+
+              padding: 0.5rem;
+
+              border-top: 1px solid ${props => props.theme.colors.text};
+            }
+          }
+        }
+      }
+    }
+  }
+
+  form {
+    overflow: hidden;
+
+    div.input-section {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 1rem;
+    }
+
+    div.input-block {
+      display: flex;
+      flex-direction: column;
+
+      max-width: 18rem;
+
+      line-height: 2rem;
+
+      input {
+        padding: 0.7rem;
+
+        border: 1px solid ${props => props.theme.colors.primary};
+        border-radius: 0.5rem;
+      }
+
+      div.all-buttons {
+        display: flex;
+        flex-direction: column;
+
+        width: 15rem;
+
+        margin: auto;
+
+        button {
+          background: none;
+          border: none;
+          border-radius: 0.5rem;
+        }
+
+        div.size-buttons {
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;
+          gap: 0.5rem;
+
+          margin-bottom: 1rem;
+
+          button.size-button {
+            padding: 0.8rem;
+
+            border: 1px solid ${props => props.theme.colors.primary};
+            color: ${props => props.theme.colors.primary};
+            opacity: 0.5;
+
+            &:hover {
+              background: ${props => props.theme.colors.primary};
+              color: white;
+              opacity: 1;
+            }
+
+            &.active {
+              opacity: 1;
+              background: ${props =>
+                transparentize(0.9, props.theme.colors.primary)};
+
+              &:hover {
+                color: ${props => props.theme.colors.primary};
+              }
+            }
+          }
+        }
+        button.reset-sizes {
+          padding: 0.8rem;
+
+          border: 1px dashed ${props => props.theme.colors.primary};
+          color: ${props => props.theme.colors.primary};
+
+          &:hover {
+            border-color: ${props =>
+              mix(0.4, '#ff0000', props.theme.colors.primary)};
+            color: ${props => mix(0.4, '#ff0000', props.theme.colors.primary)};
+          }
+        }
+      }
+    }
+
+    button[type='submit'] {
+      margin-top: 1rem;
+      width: 100%;
+      padding: 1.5rem;
+
+      background: none;
+      color: ${props => props.theme.colors.primary};
+
+      border: 1px solid ${props => props.theme.colors.primary};
+      border-radius: 0.5rem;
+
+      &:hover {
+        background: ${props => props.theme.colors.primary};
+        color: white;
+      }
     }
   }
 `
