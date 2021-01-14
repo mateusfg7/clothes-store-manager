9719e32b8a6f3846c6f2b2f1348208a4d6be68ca
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-14 14:38:57 -0300

    web: update global style
    Set outline to 'none' and add global transition time to 0.2 seconds

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -9,6 +9,8 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    outline: none;
+    transition: 0.2s;
   }
   body {
     background: ${props => props.theme.colors.background};
