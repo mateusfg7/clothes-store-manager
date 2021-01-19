4ee1876a541d55a7fd358ef3dfdca4309e80deff
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-19 16:37:16 -0300

    server: add conection with postegres db on production server

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,7 +1,18 @@
-import { Database } from "../deps.ts";
+import { config, Database } from "../deps.ts";
 
-const database = new Database("sqlite3", {
-  filepath: "./src/database/clothes_manager_database.sqlite",
-});
+const database = Deno.args.includes("--dev")
+  ? new Database(
+    "sqlite3",
+    { filepath: "./src/database/clothes_manager_database.sqlite" },
+  )
+  : new Database(
+    "postgres",
+    {
+      host: config().PG_HOST,
+      username: config().PG_USER,
+      password: config().PG_PASSWORD,
+      database: config().PG_DATABASE,
+    },
+  );
 
 export default database;
